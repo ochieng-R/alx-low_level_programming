9e8ea8e56349be20c8befd374ae99c0d032c2fab
Ramadhan Ochieng Omondi <ochiengramah@gmail.com>
2022-09-08 17:00:51 -0700

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+/**
+ * main-prints string using puts
+ * Return zero if working else a returns a nonzero
+ */
+int main (void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	returns (0);
+}
